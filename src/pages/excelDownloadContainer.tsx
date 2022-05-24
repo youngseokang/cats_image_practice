800381f11d464c5f8be45139a72f893da8d55631
kys <yskang@banaple.co.kr>
2022-05-24 10:05:04 +0900

    fileSaver 사용하여 다운로드 받는 케이스 추가

--- src/pages/excelDownloadContainer.tsx
+++ src/pages/excelDownloadContainer.tsx
@@ -1,39 +1,55 @@
 import { saveAs } from 'file-saver'
+import { useState } from 'react'
 import excelDownloadService from '../services/excelDownloadService'
 
 function ExcelDownloadContainer() {
+    const [excelState, setExcelState] = useState([])
     // TODO: PDF 다운로드
     // *1. file-saver 사용
-    // const downloadPDF = () => {
-    //     // download url 있는 경우
-    //     saveAs(
-    //         "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
-    //         "example.pdf"
-    //     )
-    // }
+    const downloadPDF1 = (fileName: string) => {
+        // download url 있는 경우
+        saveAs(
+            "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+            `${fileName}.pdf`
+        )
+    }
 
     // *2. axios 사용
-    const downloadPDF = async (fileName: string) => {
+    const downloadPDF2 = async (fileName: string) => {
         const domain = window.location.origin;
         const downloadPath = domain + "/download/pdf/" + fileName + ".pdf";
         excelDownloadService.useDownloadPDF(fileName, downloadPath)
     }
 
     // TODO: .xlsx 다운로드
-    const downloadExcel = (fileName: string, fileType: ".xlsx" | ".xls") => {
+    const downloadExcel1 = (fileName: string, fileType: ".xlsx" | ".xls") => {
         const domain = window.location.origin;
         const downloadPath = domain + "/download/excel/" + fileName + fileType;
         // console.log(downloadPath)// http://localhost:3000/download/excel/sample100.xlsx
         excelDownloadService.useDownloadExcel(fileName, downloadPath)
     }
 
+    const downloadExcel2 = (fileName: string, fileType: ".xlsx" | ".xls") => {
+        const domain = window.location.origin;
+        const downloadPath = domain + "/download/excel/" + fileName + fileType;
+        // console.log(downloadPath)// http://localhost:3000/download/excel/sample100.xlsx
+        // excelDownloadService.useDownloadExcel(fileName, downloadPath)
+        saveAs(
+            downloadPath,
+            `${fileName}.${fileType}`
+        )
+    }
+
     return (
         <div>
-            <button onClick={() => { downloadPDF("dummy") }}>dummyPDF download</button>
-            <button onClick={() => { downloadExcel("Sample100", ".xlsx") }}>Sample100.xlsx 다운로드</button>
-            <button onClick={() => { downloadExcel("Sample100", ".xls") }}>Sample100.xls 다운로드</button>
-            <button>create dummyPDF</button>
-            <button>create dummy.xlsx</button>
+            <button onClick={() => { downloadPDF1("dummy") }}>dummyPDF download</button>
+            <button onClick={() => { downloadPDF2("dummy") }}>dummyPDF download</button>
+            <button onClick={() => { downloadExcel1("Sample100", ".xlsx") }}>Sample100.xlsx 다운로드</button>
+            <button onClick={() => { downloadExcel2("Sample100", ".xls") }}>Sample100.xls 다운로드</button>
+            <div>
+                <button>create dummyPDF</button>
+                <button>create dummy.xlsx</button>
+            </div>
         </div>
     )
 }
